42088d6be588b157153a3d850e5ebd7a39f35747
pagliardini <alan.gpagliardini@gmail.com>
2024-11-21 15:30:12 -0300

    navbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Navbar from './Nav.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<StrictMode>
+  <div>
+    <Navbar />
+  </div>
+  <App />
+</StrictMode>
 )
